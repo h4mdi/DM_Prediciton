473fad318abee7e802d916639779fd6a2781ba95
hamdi bourounia <hamdi.bourounia@sesame.com.tn>
2022-01-10 02:52:20 +0100

    VS_01

--- src/main/java/com/example/dm_prediciton/MainController.java
+++ src/main/java/com/example/dm_prediciton/MainController.java
@@ -15,11 +15,14 @@ import org.supercsv.io.CsvMapWriter;
 import org.supercsv.io.ICsvBeanWriter;
 import org.supercsv.io.ICsvMapWriter;
 import org.supercsv.prefs.CsvPreference;
+import weka.attributeSelection.AttributeSelection;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.evaluation.output.prediction.AbstractOutput;
 import weka.classifiers.evaluation.output.prediction.PlainText;
+import weka.classifiers.lazy.IBk;
 import weka.classifiers.trees.J48;
+import weka.classifiers.trees.REPTree;
 import weka.classifiers.trees.RandomForest;
 import weka.core.DenseInstance;
 import weka.core.Instance;
@@ -40,11 +43,11 @@ import java.util.Map;
 @Controller
 public class MainController {
 
-    String msg ="";
+    String msg = "";
 
     @RequestMapping("/")
     public String index(Model model) {
-        model.addAttribute("house",new House()) ;
+        model.addAttribute("house", new House());
 
         return "index";
     }
@@ -55,74 +58,51 @@ public class MainController {
         //Try block to check for exceptions
         try {
 
-            // Create J48 classifier by
-            // creating object of J48 class
-            Classifier cls = new RandomForest();
+            // Create KPPV classifier
+
+            IBk cls = new IBk();
 
             // Dataset path
             String strain = "C:\\Users\\utilisa\\OneDrive\\Bureau\\PJ_DM\\ds\\train_data.csv.arff";
-
-
             // Creating bufferedreader to read the dataset
             BufferedReader br_train = new BufferedReader(new FileReader(strain));
 
             // Create dataset instances
-
-
             Instances train = new Instances(br_train);
-
             //close buffer
             br_train.close();
-
             // Set Target Class
             //Attribute att = new Attribute("price");
             train.setClassIndex(0);
-
             cls.buildClassifier(train);
-
             // Evaluating by creating object of Evaluation
             // class
             AbstractOutput output = new PlainText();
             output.setBuffer(new StringBuffer());
             output.setHeader(train);
-            output.setAttributes("first");
-
             Evaluation evaluation = new Evaluation(train);
-
             evaluation.evaluateModel(cls, train, output);
 
-            //System.out.println(evaluation.toSummaryString());
-
-
-
-            // System.out.println(evaluation.toMatrixString("\nConfusion Matrix\n========\n"));
-
-
-            // Cross Validate Model with 10 folds
-            // evaluation.crossValidateModel(j48Classifier, datasetInstances, 10,new Random(1));
-
+            // Create new instance from form data
             Instance instance = new DenseInstance(11);
             instance.setDataset(train);
-            instance.setClassMissing();
-            instance.setValue(train.attribute("neighborhood_name"),house.getNg_name());
-            instance.setValue(train.attribute("administritive_area"),house.getAdmin_area());
-            instance.setValue(train.attribute("city"),2);
-            instance.setValue(train.attribute("rooms"),house.getRooms());
-            instance.setValue(train.attribute("bathrooms"),house.getBathrooms());
-            instance.setValue(train.attribute("sqm"),house.getSqm());
-            instance.setValue(train.attribute("elevator"),house.getElev());
-            instance.setValue(train.attribute("bool"),house.getPool());
-            instance.setValue(train.attribute("driver"),house.getDriver());
-            instance.setValue(train.attribute("garden"),house.getDriver());
+            instance.setValue(train.attribute("price"), 0);
+            instance.setValue(train.attribute("neighborhood_name"), house.getNg_name());
+            instance.setValue(train.attribute("administritive_area"), house.getAdmin_area());
+            instance.setValue(train.attribute("rooms"), house.getRooms());
+            instance.setValue(train.attribute("city"), " " + house.getCity());
+            instance.setValue(train.attribute("bathrooms"), house.getBathrooms());
+            instance.setValue(train.attribute("sqm"), house.getSqm());
+            instance.setValue(train.attribute("elevator"), house.getElev());
+            instance.setValue(train.attribute("bool"), house.getPool());
+            instance.setValue(train.attribute("driver"), house.getDriver());
+            instance.setValue(train.attribute("garden"), house.getGarden());
 
-            double pred = cls.classifyInstance(instance) ;
-            String prediction=train.classAttribute().value((int)pred );
-
-
-           // System.out.println(prediction);
-            msg=prediction;
+            double pred = cls.classifyInstance(instance);
+            String prediction = train.classAttribute().value((int) pred);
+            msg = prediction;
             //System.out.println("CC"+msg);
-            evaluation.toClassDetailsString() ;
+            evaluation.toClassDetailsString();
         }
 
         // Catch block to handle the exceptions
@@ -137,50 +117,9 @@ public class MainController {
 
     @RequestMapping("/resultat")
     public String resultat(Model model) {
-        model.addAttribute("m",msg) ;
+        model.addAttribute("m", msg);
         return "resultat";
     }
 
 
-
-    @GetMapping("/users/export")
-    public void exportToCSV(HttpServletResponse response) throws IOException {
-
-        final String[] header = new String[]{"price", "neighborhood_name", "administritive_area", "city", "rooms", "bathrooms", "sqm", "elevator", "bool", "driver", "garden"};
-
-        // create the customer Maps (using the header elements for the column keys)
-        final Map<String, Object> predList = new HashMap<String, Object>();
-        predList.put(header[0], "1");
-        predList.put(header[1], "Al Munsiyah");
-        predList.put(header[2], "Riyadh");
-        predList.put(header[3], "East Riyadh");
-        predList.put(header[4], 4);
-        predList.put(header[5], 5);
-        predList.put(header[6], 240.0);
-        predList.put(header[7], 0);
-        predList.put(header[8], 0);
-        predList.put(header[9], 0);
-        predList.put(header[10], 0);
-
-
-        ICsvMapWriter mapWriter = null;
-        try {
-            mapWriter = new CsvMapWriter(new FileWriter("target/partialWriteWithCsvMapWriter.csv"),
-                    CsvPreference.STANDARD_PREFERENCE);
-
-            final CellProcessor[] processors = new CellProcessor[]{ new NotNull(), new NotNull(),
-                    new NotNull(),  new NotNull(),  new NotNull(), new NotNull(), new NotNull(), new NotNull(), new NotNull(), new NotNull(), new NotNull()};
-
-            // write the header
-            mapWriter.writeHeader(header);
-
-            // write the customer Maps
-            mapWriter.write(predList, header, processors);
-
-        } finally {
-            if (mapWriter != null) {
-                mapWriter.close();
-            }
-        }
-    }
 }
